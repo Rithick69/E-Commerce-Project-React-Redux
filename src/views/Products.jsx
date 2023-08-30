01749f4d59489e086ab0fb073534763ab88a3f6d
Rithick69 <rithickcy19@gmail.com>
2023-08-31 00:03:11 +0530

    Started work on Products page, WIP.

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,20 +1,45 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
+import { styled } from 'styled-components';
+import Sort from '../components/Sort';
+import FilterSection from '../components/FilterSection';
+import ProductList from '../components/ProductList';
 
 const Products = () => {
-	const { products, isLoading } = useSelector((store) => {
-		return store.products;
-	});
+	// const { products, isLoading } = useSelector((store) => {
+	// 	return store.products;
+	// });
 
-	if (!isLoading) {
-		return (
-			<div>
-				<p>Data: {isLoading}</p>
-				{products.map((curr, idx) => {
-					return <p key={idx}>{curr.name}</p>;
-				})}
+	// if (!isLoading) {
+	return (
+		<Wrapper>
+			<div className="container grid grid-filter-column">
+				<div>
+					<FilterSection />
+				</div>
+				<section className="product-view--sort">
+					<div className="sort-filter">
+						<Sort />
+					</div>
+					<div className="main-product">
+						<ProductList />
+					</div>
+				</section>
 			</div>
-		);
-	}
+		</Wrapper>
+	);
+	// }
 };
+
+const Wrapper = styled.section`
+	.grid-filter-column {
+		grid-template-columns: 0.2fr 1fr;
+	}
+
+	@media (max-width: ${({ theme }) => theme.media.mobile}) {
+		.grid-filter-column {
+			grid-template-columns: 1fr;
+		}
+	}
+`;
 
 export default Products;
