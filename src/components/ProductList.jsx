323041003291914c9b3b88055fccdfa6c1a4a8d3
Rithick69 <rithickcy19@gmail.com>
2023-09-02 00:20:20 +0530

    Displayed API response in Products page. WIP

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,7 +7,10 @@ const ProductList = () => {
 	const { products, isLoading } = useSelector((store) => {
 		return store.products;
 	});
-	console.log(products);
+
+	if (isLoading) {
+		return <div>Loading .....</div>;
+	}
 	return (
 		<Wrapper>
 			<div className="container">
@@ -22,11 +25,85 @@ const ProductList = () => {
 };
 
 const Wrapper = styled.section`
-	padding: 9rem 0;
+	padding: 5rem 0;
 	background-color: ${({ theme }) => theme.colors.bg};
 
 	.container {
-		max-width: 120rem;
+		padding: 0 2rem;
+	}
+
+	.grid {
+		gap: 5rem;
+	}
+
+	figure {
+		width: auto;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		overflow: hidden;
+		transition: all 0.5s linear;
+		&::after {
+			content: '';
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 0%;
+			height: 100%;
+			background-color: rgba(0, 0, 0, 0.5);
+			transition: all 0.2s linear;
+			cursor: pointer;
+		}
+		&:hover::after {
+			width: 100%;
+		}
+		&:hover img {
+			transform: scale(1.2);
+		}
+		img {
+			max-width: 90%;
+			margin-top: 1.5rem;
+			height: 15rem;
+			transition: all 0.2s linear;
+		}
+
+		.caption {
+			position: absolute;
+			top: 15%;
+			right: 10%;
+			text-transform: uppercase;
+			background-color: ${({ theme }) => theme.colors.bg};
+			color: ${({ theme }) => theme.colors.helper};
+			padding: 0.8rem 2rem;
+			font-size: 1.2rem;
+			border-radius: 2rem;
+		}
+	}
+
+	.card {
+		background-color: #fff;
+		border-radius: 1rem;
+
+		.card-data {
+			padding: 0 2rem;
+		}
+
+		.card-data-flex {
+			margin: 2rem 0;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+		}
+
+		h3 {
+			color: ${({ theme }) => theme.colors.text};
+			text-transform: capitalize;
+		}
+
+		.card-data--price {
+			color: ${({ theme }) => theme.colors.helper};
+		}
 	}
 `;
 
